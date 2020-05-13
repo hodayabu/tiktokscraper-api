2ce211ef42171f2a9448a03aa305a72d85e243c2
bhodaya <bhodaya@videocites.com>
2020-05-13 21:37:02 +0300

    num

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/tiktok/search', function(req, res){
             tag = req.query.tag;
             num = parseInt(req.query.num);
             proxy = req.query.proxy;
-            const posts = await TikTokScraper.hashtag(tag, { number: 1, filetype:`json`});
+            const posts = await TikTokScraper.hashtag(tag, { number: num, filetype:`json`});
 
             console.log(posts);
             res.send(posts)
